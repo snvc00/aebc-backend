02acab00cdb67db611b79a1c4513d36241ec0e3e
Santiago Valle <snvc00@gmail.com>
2021-05-26 16:35:00 -0500

    Implement functions to interact with credit card table

--- src/utils/database.py
+++ src/utils/database.py
@@ -1,5 +1,6 @@
 import settings
 from models.client import Client
+from models.credit_card import CreditCard
 from sqlalchemy import create_engine
 from sqlalchemy.orm.session import Session
 
@@ -54,7 +55,7 @@ class ClientDB():
 
         session.commit()
 
-    def delete_by_curp(curp: str):
+    def delete_by_curp(curp: str) -> None:
         engine = create_engine(settings.DB_URL)
         session = Session(engine, future=True)
         affected_rows = session.query(Client).filter_by(
@@ -62,5 +63,65 @@ class ClientDB():
 
         if affected_rows == 0:
             raise ApiException("Client not found", 404)
+
+        session.commit()
+
+
+class CreditCardDB():
+    """
+    """
+
+    def fetch_all() -> list[dict]:
+        engine = create_engine(settings.DB_URL)
+        session = Session(engine, future=True)
+        credit_cards = []
+
+        for credit_card_instance in session.query(CreditCard):
+            credit_card = credit_card_instance.__dict__
+            credit_card.pop("_sa_instance_state", None)
+            credit_cards.append(credit_card)
+
+        return credit_cards
+
+    def fetch_by_id(id: str) -> dict:
+        engine = create_engine(settings.DB_URL)
+        session = Session(engine, future=True)
+        credit_card_instance = session.query(CreditCard).filter_by(id=id).first()
+
+        if credit_card_instance is None:
+            raise ApiException("Credit card not found", 404)
+
+        credit_card = credit_card_instance.__dict__
+        credit_card.pop("_sa_instance_state", None)
+
+        return credit_card
+
+    def insert(new_credit_card: dict) -> None:
+        engine = create_engine(settings.DB_URL)
+        session = Session(engine, future=True)
+        new_credit_card = CreditCard(data=new_credit_card)
+
+        session.add(new_credit_card)
+        session.commit()
+
+    def update_by_id(id: str, new_values: dict) -> None:
+        engine = create_engine(settings.DB_URL)
+        session = Session(engine, future=True)
+        affected_rows = session.query(CreditCard).filter_by(
+            id=id).update(new_values, synchronize_session="fetch")
+
+        if affected_rows == 0:
+            raise ApiException("Credit card not found", 404)
+
+        session.commit()
+
+    def delete_by_id(id: str) -> None:
+        engine = create_engine(settings.DB_URL)
+        session = Session(engine, future=True)
+        affected_rows = session.query(CreditCard).filter_by(
+            id=id).delete(synchronize_session="fetch")
+
+        if affected_rows == 0:
+            raise ApiException("Credit card not found", 404)
 
         session.commit()
