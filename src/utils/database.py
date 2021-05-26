1dd5a87ab5d204f2ac343ce0e9361c5d1dcb32e0
Santiago Valle <snvc00@gmail.com>
2021-05-26 17:22:04 -0500

    Implement class to interact with benefit table

--- src/utils/database.py
+++ src/utils/database.py
@@ -1,3 +1,4 @@
+from models.benefit import Benefit
 import settings
 from models.client import Client
 from models.credit_card import CreditCard
@@ -123,5 +124,65 @@ class CreditCardDB():
 
         if affected_rows == 0:
             raise ApiException("Credit card not found", 404)
+
+        session.commit()
+
+class BenefitTable():
+    """
+    """
+
+    def fetch_all() -> list[dict]:
+        engine = create_engine(settings.DB_URL)
+        session = Session(engine, future=True)
+        benefits = []
+
+        for benefit_instance in session.query(Benefit):
+            benefit = benefit_instance.__dict__
+            print(benefit)
+            benefit.pop("_sa_instance_state", None)
+            benefits.append(benefit)
+
+        return benefits
+    
+    def fetch_by_id(id: str) -> dict:
+        engine = create_engine(settings.DB_URL)
+        session = Session(engine, future=True)
+        benefit_instance = session.query(Benefit).filter_by(id=id).first()
+
+        if benefit_instance is None:
+            raise ApiException("Benefit not found", 404)
+
+        benefit = benefit_instance.__dict__
+        benefit.pop("_sa_instance_state", None)
+
+        return benefit
+
+    def insert(new_benefit: dict) -> None:
+        engine = create_engine(settings.DB_URL)
+        session = Session(engine, future=True)
+        new_benefit = Benefit(data=new_benefit)
+
+        session.add(new_benefit)
+        session.commit()
+
+    def update_by_id(id: str, new_values: dict) -> None:
+        engine = create_engine(settings.DB_URL)
+        session = Session(engine, future=True)
+        affected_rows = session.query(Benefit).filter_by(
+            id=id).update(new_values, synchronize_session="fetch")
+
+        if affected_rows == 0:
+            raise ApiException("Benefit not found", 404)
+
+        session.commit()
+
+    def delete_by_id(id: str) -> None:
+        engine = create_engine(settings.DB_URL)
+        session = Session(engine, future=True)
+        affected_rows = session.query(Benefit).filter_by(
+            id=id).delete(synchronize_session="fetch")
+
+        if affected_rows == 0:
+            raise ApiException("Benefit not found", 404)
 
         session.commit()
