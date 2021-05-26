022de348c1f057c5fa5bb240ed16b36d78cc7b1d
Santiago Valle <snvc00@gmail.com>
2021-05-26 15:59:57 -0500

    Create model for client table

---
+++ src/models/client.py
@@ -0,0 +1,32 @@
+from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class Client(Base):
+    """
+    """
+    __tablename__ = "client"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    rfc = Column(String)
+    password = Column(String)
+    monthly_income = Column(Integer)
+    has_credit = Column(Boolean)
+    address = Column(String)
+    city = Column(String)
+    state = Column(String)
+    neighborhood = Column(String)
+    is_active = Column(Boolean)
+    curp = Column(String)
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__()
+
+        if kwargs.keys().__contains__("data"):
+            self.__dict__.update(kwargs.get("data"))
+
+    def __repr__(self) -> str:
+        return "id: {}, curp: {}".format(self.id, self.curp)
