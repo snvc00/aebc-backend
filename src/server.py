5342b83864bdcfaff7353fefdeffda3f7f230af0
Santiago Valle <snvc00@gmail.com>
2021-05-26 16:35:27 -0500

    Implement routes for credit cards

--- src/server.py
+++ src/server.py
@@ -1,7 +1,8 @@
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
-from endpoints.clients import delete_client, fetch_clients, insert_client, fetch_client, update_client
+
 import settings
+from endpoints import clients, credit_cards
 
 server = Flask(__name__)
 server.config.from_object(settings)
@@ -11,9 +12,9 @@ CORS(server, support_credentials=True)
 @server.route("/api/clients", methods=["GET", "POST"])
 def clients_route():
     if request.method == "GET":
-        response = fetch_clients()
+        response = clients.fetch_clients()
     elif request.method == "POST":
-        response = insert_client(request)
+        response = clients.insert_client(request)
 
     return response
 
@@ -21,11 +22,11 @@ def clients_route():
 @server.route("/api/clients/<curp>", methods=["GET", "PATCH", "DELETE"])
 def client_route(curp: str):
     if request.method == "GET":
-        response = fetch_client(curp)
+        response = clients.fetch_client(curp)
     elif request.method == "PATCH":
-        response = update_client(curp, request)
+        response = clients.update_client(curp, request)
     elif request.method == "DELETE":
-        response = delete_client(curp)
+        response = clients.delete_client(curp)
 
     return response
 
@@ -33,23 +34,24 @@ def client_route(curp: str):
 @server.route("/api/cards", methods=["GET", "POST"])
 def credit_cards_route():
     if request.method == "GET":
-        pass
+        response = credit_cards.fetch_credit_cards()
     elif request.method == "POST":
-        pass
+        print(type(request))
+        response = credit_cards.insert_credit_card(request)
 
-    return 0
+    return response
 
 
 @server.route("/api/cards/<id>", methods=["GET", "PATCH", "DELETE"])
 def credit_card_route(id: str):
     if request.method == "GET":
-        pass
+        response = credit_cards.fetch_credit_card(id)
     elif request.method == "PATCH":
-        pass
+        response = credit_cards.update_credit_card(id, request)
     elif request.method == "DELETE":
-        pass
+        response = credit_cards.delete_credit_card(id)
 
-    return 0
+    return response
 
 
 @server.route("/api/benefits", methods=["GET"])
