709e2ac326ee4447270e1519ff093eca612f3f5c
Santiago Valle <snvc00@gmail.com>
2021-06-29 13:45:24 -0500

    Update defaultsuperuser command and docker compose testing env

--- aecb/api/management/commands/defaultsuperuser.py
+++ aecb/api/management/commands/defaultsuperuser.py
@@ -8,8 +8,8 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         DJANGO_SU_PASSWORD = os.environ.get("DJANGO_SU_PASSWORD")
 
-        if User.objects.get(username="aecb-admin") is None:
+        try:
             User.objects.create_superuser("aecb-admin", "admin@aecb.com", DJANGO_SU_PASSWORD)
             print("aecb-admin user created")
-        else:
-            print("aecb-admin already exists")
+        except Exception as e:
+            print(str(e))
